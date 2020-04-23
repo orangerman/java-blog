690dc465a49e7662c76bd8f1e8453a6cc63e7818
javafan <1057600474@qq.com>
2020-04-23 14:52:24 +0800

    web init thymeleaf crud

--- spring-boot-web-restfulcrud/src/main/java/com/javafan/springbootweb/controller/HelloController.java
+++ spring-boot-web-restfulcrud/src/main/java/com/javafan/springbootweb/controller/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+import java.util.Map;
+
 /**
  * @author javafan
  */
@@ -17,6 +20,17 @@ public class HelloController {
     }
 
 
+    /**
+     * 查出一些数据 在页面展示
+     *
+     * @return SUCCESS页面
+     */
+    @RequestMapping("/success")
+    public String success(Map<String, Object> map) {
+        map.put("hello", "<h1>你好</h1>");
+        map.put("users", Arrays.asList("zhangsan", "lisi", "wangwu"));
+        return "success";
+    }
 
 
 }
